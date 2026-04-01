31fb14f78f9241c42b9bad6667809605e4bcdb9a
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-04-02 01:20:03 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
               <h2 className="text-[#455970]/30 text-sm font-black uppercase tracking-[0.4em]">La Filosofia</h2>
               <h3 className="text-5xl md:text-7xl font-bold leading-tight tracking-tighter">
                 Una cucina<br />
-                <span className="text-[#ffefcc] bg-[#455970] px-4 py-1 inline-block">Gourmet</span>
+                <span className="text-[#ffefcc] bg-[#455970] px-4 py-1 inline-block">Gourmet.</span>
               </h3>
               <p className="text-xl text-gray-500 leading-relaxed max-w-md">
                 Qui, offriamo le migliori pizze e pinse, preparate con ingredienti di alta qualità e originalità. Il nostro menu è stato progettato per soddisfare le esigenze di ogni palato, dai più tradizionali ai più avventurosi. Se sei alla ricerca di un ristorante che offra un'esperienza gastronomica unica, allora non perderti la nostra.
