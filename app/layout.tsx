615b5bf5c482135cb56a6184b8198f31f43da9fc
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-04-02 01:14:28 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import CookieBanner from "./components/CookieBanner"; // Assicurati che il percorso sia corretto (es. se è in /components/CookieBanner.tsx)
+import CookieBanner from "./components/CookieBanner";
 
 export const metadata = {
   title: "Civico 2 | Ristorante",
@@ -21,9 +21,7 @@ export default function RootLayout({
           {/* pt-20 serve per non coprire il contenuto con l'header fisso */}
           {children}
         </main>
-        <footer />
-        
-        {/* Il Cookie Banner viene caricato qui per essere globale */}
+        <Footer />
         <CookieBanner />
       </body>
     </html>
